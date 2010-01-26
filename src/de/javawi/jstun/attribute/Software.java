ffd9e13d46e8fb79ecfaf3a523f4d8c56e8090be
asymmetric <asymmetric@autistici.org>
2010-01-26 19:05:38 +0100

    fixed a couple bugs & inefficiencies

--- src/de/javawi/jstun/attribute/Software.java
+++ src/de/javawi/jstun/attribute/Software.java
@@ -17,17 +17,20 @@ public class Software extends AbstractMessageAttribute {
 	public byte[] getBytes() throws UtilityException {
 		byte[] result = new byte[COMMONHEADERSIZE + software.length()];
 		
+		int length = software.length();
+
 		// common attribute header
 		// type
 		System.arraycopy(Utility.integerToTwoBytes(MessageAttributeType.Software.getEncoding()), 0, result, 0, 2);
 		// length
-		System.arraycopy(Utility.integerToTwoBytes(software.length()), 0, result, 2, 2);
+		System.arraycopy(Utility.integerToTwoBytes(length), 0, result, 2, 2);
+
+		if (length % ALIGNMENT != 0)
+			length += ALIGNMENT - (length % ALIGNMENT);
 		
-		int padding = software.getBytes().length % ALIGNMENT;
 		// software attribute header
-		System.arraycopy(software.getBytes(), 0, result, 4, software.length());
-		// add padding
-		System.arraycopy(null, 0, result, software.length(), padding);
+		System.arraycopy(software.getBytes(), 0, result, 4, length);
+
 		return result;
 	}
 
