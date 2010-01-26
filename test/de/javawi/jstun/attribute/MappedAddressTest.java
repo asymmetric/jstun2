4164848db1f424d090b36e8c9ac82ecc471275db
asymmetric <asymmetric@autistici.org>
2010-01-26 17:48:12 +0100

    testing the XORMapped attribute, doesn't work yet

--- test/de/javawi/jstun/attribute/MappedAddressTest.java
+++ test/de/javawi/jstun/attribute/MappedAddressTest.java
@@ -13,29 +13,46 @@ package de.javawi.jstun.attribute;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
+import de.javawi.jstun.attribute.AbstractMessageAttribute.MessageAttributeType;
+import de.javawi.jstun.header.MessageHeaderInterface;
+import de.javawi.jstun.util.Utility;
+
 public class MappedAddressTest extends TestCase {
 	MappedXORMapped map;
+	MappedXORMapped xor;
+	
+	int port = 63584;
+	int address = 1413015884;
+	
 	byte[] data;
 	public MappedAddressTest(String mesg) {
 		super(mesg);
 	}
 	
 	public void setUp() throws Exception {
+		byte asd = -8;
+		byte omar = 96;
+		int asdomar = (asd << 8) | omar;
 		data = new byte[8];
-		data[0] = 0;
-		data[1] = 1;
-		data[2] = -8;
+		data[0] = 0; // padding
+		data[1] = 1; // IPv4
+		data[2] = -8; // port
 		data[3] = 96;
-		data[4] = 84;
+		data[4] = 84; // address
 		data[5] = 56;
 		data[6] = -23;
 		data[7] = 76;
-		map = new MappedXORMapped(data);
+		map = new MappedXORMapped(MessageAttributeType.MappedAddress, data);
+		
+		xor = new MappedXORMapped(data);
 	}
 
 	/*
 	 * Test method for 'de.javawi.jstun.attribute.MappedXORMapped.MappedXORMapped()'
 	 */
+	@Test
 	public void testMappedAddress() {
 		new MappedXORMapped();
 	}
@@ -43,22 +60,44 @@ public class MappedAddressTest extends TestCase {
 	/*
 	 * Test method for 'de.javawi.jstun.attribute.MappedResponseChangedSourceAddressReflectedFrom.getBytes()'
 	 */
+	@Test
 	public void testGetBytes() {
 		try {
-			byte[] result = map.getBytes();
+			byte[] resultMap = map.getBytes();
 
-			assertTrue(result[0] == 0);
-			assertTrue(result[1] == 1);
-			assertTrue(result[2] == 0);
-			assertTrue(result[3] == 8);
-			assertTrue(result[4] == data[0]);
-			assertTrue(result[5] == data[1]);
-			assertTrue(result[6] == data[2]);
-			assertTrue(result[7] == data[3]);
-			assertTrue(result[8] == data[4]);
-			assertTrue(result[9] == data[5]);
-			assertTrue(result[10] == data[6]);
-			assertTrue(result[11] == data[7]);
+			assertTrue(resultMap[0] == 0); // type
+			assertTrue(resultMap[1] == 1);
+			assertTrue(resultMap[2] == 0); // length
+			assertTrue(resultMap[3] == 8);
+			assertTrue(resultMap[4] == data[0]); // attribute value
+			assertTrue(resultMap[5] == data[1]);
+			assertTrue(resultMap[6] == data[2]);
+			assertTrue(resultMap[7] == data[3]);
+			assertTrue(resultMap[8] == data[4]);
+			assertTrue(resultMap[9] == data[5]);
+			assertTrue(resultMap[10] == data[6]);
+			assertTrue(resultMap[11] == data[7]);
+			
+			byte[] resultXor = xor.getBytes();
+			
+			int xPort = port ^ MessageHeaderInterface.MAGICCOOKIE;
+			int xAddr = address ^ MessageHeaderInterface.MAGICCOOKIE;
+			
+			byte[] portByte = Utility.integerToTwoBytes(xPort);
+			byte[] addressByte = Utility.integerToFourBytes(xAddr);
+			
+			assertTrue(resultXor[0] == 0); // type
+			assertTrue(resultXor[1] == 0x20); // XORMapped type = 0x20
+			assertTrue(resultXor[2] == 0); // length
+			assertTrue(resultXor[3] == 8);
+			assertTrue(resultXor[4] == data[0]);
+			assertTrue(resultXor[5] == data[1]);
+			assertTrue(resultXor[6] == portByte[0]); // X-Port
+			assertTrue(resultXor[7] == portByte[1]);
+			assertTrue(resultXor[8] == addressByte[0]);
+			assertTrue(resultXor[9] == addressByte[1]);
+			assertTrue(resultXor[10] == addressByte[2]);
+			assertTrue(resultXor[11] == addressByte[3]);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
