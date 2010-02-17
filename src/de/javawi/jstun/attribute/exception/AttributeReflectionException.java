91c6f16a2f284b065c57bfc07e71d0b7314955dc
asymmetric <asymmetric@autistici.org>
2010-02-17 16:55:20 +0100

    actually added the exception, which i had forgotten about

---
+++ src/de/javawi/jstun/attribute/exception/AttributeReflectionException.java
@@ -0,0 +1,29 @@
+package de.javawi.jstun.attribute.exception;
+
+public class AttributeReflectionException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 164965008874776472L;
+
+    public AttributeReflectionException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public AttributeReflectionException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AttributeReflectionException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AttributeReflectionException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
